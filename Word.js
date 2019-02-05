70e747b9702ce0e5b9e92ed050d596c22bbc394e
heincm <chris.m.hein@gmail.com>
2019-02-04 21:28:44 -0500

    Letter objects push to array as expected

--- Word.js
+++ Word.js
@@ -1,3 +1,20 @@
 const Letter = require('./Letter')
 
-let 
+let Word = function (guessWord) {
+    this.guessWord = guessWord;
+    this.array = [];
+    for (let i = 0; i < guessWord.length; i++) {
+        this.array.push(new Letter(guessWord[i]));
+    }
+}
+
+Word.prototype.returnString = function () { debugger;
+    for (let g = 0; g < this.array.length; g++) {
+        console.log(this.array[g].guessed());
+    }
+
+};
+
+let hello = new Word("hello");
+console.log(hello.array)
+hello.returnString();
