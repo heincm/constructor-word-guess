721df7910195acbdc7f9c6fa6a8844453e2a512b
heincm <chris.m.hein@gmail.com>
2019-02-04 21:28:23 -0500

    refactored with prototypes

--- Letter.js
+++ Letter.js
@@ -2,18 +2,20 @@
 let Letter = function (string) {
     this.string = string;
     this.boolean = false;
-    this.guessed = function () {
-        if (this.boolean === true){
-            return this.string;
-        } else {
-            return "_";
-        }
-    };
-    this.takeCharacter = function(character) {
-        if (character === this.string){
-            this.boolean = true;
-        }
+};
+
+Letter.prototype.guessed = function () {
+    if (this.boolean === true){
+        return this.string;
+    } else {
+        return "_";
     }
 };
+
+Letter.prototype.takeCharacter = function(character) {
+    if (character === this.string){
+        this.boolean = true;
+    }
+}
 
 module.exports = Letter;
