f70bba158d59580739131364928f1b7678aefcee
heincm <chris.m.hein@gmail.com>
2019-02-09 18:09:20 -0500

    added chalk to add color to conosle display

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Word = require('./Word');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 
 let wordBank = ['chainring', 'sprocket', 'clips', 'saddle', 'handlebars', 'drafting', 'endo', 'fixie',
   'derailleur', 'crankarm', 'frame', 'helmet', 'peloton', 'presta', 'roadie', 'schrader', 'spokes',
@@ -8,6 +9,8 @@ let wordBank = ['chainring', 'sprocket', 'clips', 'saddle', 'handlebars', 'draft
 let randNum = Math.floor(Math.random() * wordBank.length) + 1;
 let remainingGuesses = 10;
 remainingGuesses;
+let correctGuesses = [];
+let allGuesses = [];
 
 function checkRound() {
   if (remainingGuesses = 0) {
@@ -17,10 +20,17 @@ function checkRound() {
 };
 
 
+
 function playRound() {
   let currentWord = new Word(wordBank[randNum])
-  console.log(currentWord);  
-  currentWord.returnString();
+  console.log(" ")
+  console.log(currentWord.guessWord);
+  console.log(" ")
+    for (let q = 0; q < correctGuesses.length; q++) {
+      currentWord.checkLetter(correctGuesses[q]);
+    };
+    currentWord.returnString();
+    console.log(" ")
   inquirer
     .prompt([
       {
@@ -30,8 +40,13 @@ function playRound() {
       }
     ])
     .then(answers => {
-      currentWord.checkLetter(answers.q1)
-      currentWord.returnString();
+      if (currentWord.guessWord.includes(answers.q1)) {
+        correctGuesses.push(answers.q1);
+        console.log(chalk.green("Correct-a-mundo, broski!"));
+      } else {
+        console.log(chalk.red("You wrong, bruh! Try again!"))
+      }
+      console.log(" ");
       checkRound();
     });
 }
