87480b32f3c756ff520d90e6f0a357903b0716ec
heincm <chris.m.hein@gmail.com>
2019-02-09 20:24:11 -0500

    added logic for game over. started logic for winning round

--- index.js
+++ index.js
@@ -8,29 +8,34 @@ let wordBank = ['chainring', 'sprocket', 'clips', 'saddle', 'handlebars', 'draft
 
 let randNum = Math.floor(Math.random() * wordBank.length) + 1;
 let remainingGuesses = 10;
-remainingGuesses;
 let correctGuesses = [];
 let allGuesses = [];
+let remainingLetters;
 
 function checkRound() {
-  if (remainingGuesses = 0) {
-    console.log("game over")
+  if (remainingLetters === 0){
+    console.log("Winner!")
+    process.exit();
   }
-  playRound();
+  if (remainingGuesses === 0) {
+    console.log(chalk.red.bold.inverse("game over"))
+  } else {
+    playRound();
+  }
 };
 
 
 
-function playRound() {
+function playRound() { debugger;
   let currentWord = new Word(wordBank[randNum])
-  console.log(" ")
-  console.log(currentWord.guessWord);
-  console.log(" ")
-    for (let q = 0; q < correctGuesses.length; q++) {
-      currentWord.checkLetter(correctGuesses[q]);
-    };
-    currentWord.returnString();
-    console.log(" ")
+  remainingLetters = currentWord.guessWord.length
+
+  for (let q = 0; q < correctGuesses.length; q++) {
+    currentWord.checkLetter(correctGuesses[q]);
+  };
+
+  currentWord.returnString();
+
   inquirer
     .prompt([
       {
@@ -40,13 +45,24 @@ function playRound() {
       }
     ])
     .then(answers => {
-      if (currentWord.guessWord.includes(answers.q1)) {
-        correctGuesses.push(answers.q1);
-        console.log(chalk.green("Correct-a-mundo, broski!"));
+      if (!allGuesses.includes(answers.q1)) {
+        if (currentWord.guessWord.includes(answers.q1)) {
+          correctGuesses.push(answers.q1);
+          console.log(chalk.green("Correct-a-mundo, broski!"));
+          console.log("remaining letters", remainingLetters);
+          remainingLetters--;
+        } else {
+          remainingGuesses--;
+          if (remainingGuesses > 0) {
+            console.log(chalk.red("Wrong answer, bruh! Try again! Remaining Guesses: ", remainingGuesses))
+          } else {
+            console.log(chalk.red("Too many wrong guesses"))
+          }
+        }
+        allGuesses.push(answers.q1);
       } else {
-        console.log(chalk.red("You wrong, bruh! Try again!"))
+        console.log(chalk.blue("You already guessed that letter. Guess another letter"));
       }
-      console.log(" ");
       checkRound();
     });
 }
