f3c9f598a3672c865ff2166933bb60178587abb2
heincm <chris.m.hein@gmail.com>
2019-02-10 21:17:28 -0500

    Cleaned up code. Allow only lowercase letter responses. Edited logged messages

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const Word = require('./Word');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
-const log = console.log
+const log = console.log;
 
 let wordBank = ['chainring', 'sprocket', 'clips', 'saddle', 'handlebars', 'drafting', 'endo', 'fixie',
   'derailleur', 'crankarm', 'frame', 'helmet', 'peloton', 'presta', 'roadie', 'schrader', 'spokes',
-  'stem', 'tubeless', 'wheelie', 'flat', 'aero',]
+  'stem', 'tubeless', 'wheelie', 'flat', 'aero',];
 
 let remainingGuesses;
 let correctGuesses;
 let allGuesses;
 let remainingLetters;
 let currentWord;
+const invalidResponse = /[^a-z]/;
 
 function checkRound() {
   if (remainingLetters === 0) {
@@ -20,10 +21,10 @@ function checkRound() {
     };
     currentWord.returnString();
     wordBank.splice(wordBank.indexOf(currentWord.guessWord));
-    log(chalk.green.bold("WINNER! \nWINNER! \nWINNER!"));
+    log(chalk.green.bold("WINNER! \nWINNER! \nWINNER! \nYou guessed the word!"));
     startGame();
   } else if (remainingGuesses === 0) {
-    log(chalk.red("Too many wrong guesses ") + chalk.red.bold.inverse("GAME OVER!"));
+    log(chalk.red("Too many incorrect guesses ") + chalk.red.bold.inverse("GAME OVER!"));
     startGame();
   } else {
     playRound();
@@ -73,15 +74,20 @@ function playRound() {
       }
     ])
     .then(answers => {
-      if (!isNaN(answers.q1)) {
-        log(chalk.yellow("No numbers, bro!"));
+      if (answers.q1.match(invalidResponse)){ 
+        log(chalk.yellow("Please enter only lower case letters"))
+        playRound();
+        return;
+      }
+      if (answers.q1.length > 1) {
+        log(chalk.yellow("Please enter only one character at a time"));
         playRound();
         return;
       }
       if (!allGuesses.includes(answers.q1)) {
         if (currentWord.guessWord.includes(answers.q1)) {
           correctGuesses.push(answers.q1);
-          log(chalk.green("Correct-a-mundo, broski!"));
+          log(chalk.green("Correct!", answers.q1, "is used to spell this word!"));
           for (let n = 0; n < currentWord.guessWord.length; n++) {
             if (answers.q1 === currentWord.guessWord[n]) {
               remainingLetters--;
@@ -90,15 +96,16 @@ function playRound() {
         } else {
           remainingGuesses--;
           if (remainingGuesses > 0) {
-            log(chalk.red("Wrong answer, bruh! Try again! Remaining Guesses:", remainingGuesses));
+            log(chalk.red("Incorrect guess!", answers.q1, "is not used to spell this word. Try again! Remaining Guesses:", remainingGuesses));
           }
         }
         allGuesses.push(answers.q1);
       } else {
-        log(chalk.blue("You already guessed that letter, brosef. Guess another letter."));
+        log(chalk.blue("You already guessed that letter. Guess another letter."));
       }
       checkRound();
     });
 };
 
+log(chalk.blueBright("Welcome to Bicycle Word Guess!"));
 startGame();
